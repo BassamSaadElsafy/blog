f0b903de8ac3851e7e9e8839180c6080cd76654d
Bassam Saad <bassamsaad771@gmail.com>
2021-03-23 02:26:11 +0200

    add middleware auth group for posts

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use Whoops\Run;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,22 +16,28 @@ use App\Http\Controllers\PostController;
 */
 
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+//group route for posts using auth middleware
 
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::middleware('auth')->group(function () {
+    
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
-Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+
+    Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+    Route::post('/posts/restore', [PostController::class, 'restore'])->name('posts.restore');
+
+    Route::get('/posts/show/ajax', [PostController::class, 'get_post_response'])->name('posts.ajax_show');
+
+});
 
 
-Route::post('/posts/restore', [PostController::class, 'restore'])->name('posts.restore');
-
-Route::get('/posts/show/ajax', [PostController::class, 'get_post_response'])->name('posts.ajax_show');
-
-Auth::routes();
+Auth::routes();             //contains all routes related to Auth Middleware
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
